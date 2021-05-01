584f3878491506ad8e6126c9f5dd7540919a912e
Mohammad Belal <king.cseiu@gmail.com>
2021-05-01 13:28:22 +0600

    initial scope test var let const

---
+++ let_var_const.js
@@ -0,0 +1,25 @@
+// function test() {
+//     for (var i = 0; i < 3; i++) {
+//         console.log("Hello World!");      // print HelloWorld! 3 times
+//     }
+//     console.log(i);
+// }
+// test();
+
+// function test() {
+//     for (let i = 0; i < 3; i++) {
+//         console.log("Hellow World!");           //print Hello World! & error found
+//     }
+//     console.log(i);
+// }
+// test();
+
+// if (true) {
+//     var color = "purple";         // print purple
+// }
+// console.log(color);
+
+if (true) {
+    const color = "purple"; // error found, use let also found error 
+}
+console.log(color);

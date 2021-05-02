ffe1ca3f4b249153a67a0a8caabaa2be6296bb8f
Mohammad Belal <king.cseiu@gmail.com>
2021-05-02 10:54:50 +0600

    javaScript object

---
+++ object.js
@@ -0,0 +1,29 @@
+person = {
+        fname: "Mohammad",
+        lname: "Belal",
+        age: 32,
+        // fullName: function() {
+        //     return this.fname + " " + this.lname
+        // }
+    }
+    // console.log(person.fullName());
+
+// var detail = "";
+
+// for (x in person) { // for...in loop
+//     detail += person[x] + " ";
+// }
+// console.log(detail);
+
+// person.nationality = "Banglaseshi"; //Adding new properties
+
+// delete person.age; //Deleting Properties
+
+// var message = "Hello world!";
+// var x = message.toUpperCase();  //Using Built-In Methods
+// console.log(x);
+
+person.name = function() {
+    return this.fname + " " + this.lname;
+}
+console.log(person.name());

b1613010c5e8156044513df48f0d5c76d3ff54b3
Mohammad Belal <king.cseiu@gmail.com>
2021-05-11 22:51:38 +0600

    constructor function

--- function.js
+++ function.js
@@ -1,3 +1,11 @@
-(function() { //Self-Invoking Functions
-    console.log("Hi...I can invoke me!!");
-})();
+// (function() { //Self-Invoking Functions
+//     console.log("Hi...I can invoke me!!");
+// })();
+
+function myFunction(arg1, arg2) { //myFunction is a function constructor
+    this.firstName = arg1;
+    this.lastName = arg2;
+}
+
+var x = new myFunction("John", "Doe")
+console.log(x.firstName);

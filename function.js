bd1a27f87632967c27c97d25e3bbe1548d8043fb
Mohammad Belal <king.cseiu@gmail.com>
2021-05-12 23:28:17 +0600

    closures

--- function.js
+++ function.js
@@ -2,10 +2,19 @@
 //     console.log("Hi...I can invoke me!!");
 // })();
 
-function myFunction(arg1, arg2) { //myFunction is a function constructor
-    this.firstName = arg1;
-    this.lastName = arg2;
-}
+// function myFunction(arg1, arg2) { //myFunction is a function constructor
+//     this.firstName = arg1;
+//     this.lastName = arg2;
+// }
 
-var x = new myFunction("John", "Doe")
-console.log(x.firstName);
+// var x = new myFunction("John", "Doe")
+// console.log(x.firstName);
+
+var add = (function() { //JavaScript Closures, Counting with a local variable.
+    var counter = 0;
+    return function() { counter += 1; return counter; }
+})();
+
+function myFunction() {
+    console.log(add());
+}

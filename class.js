c2373575b4d7080515312abf9034c4f7d10cf39d
Mohammad Belal <king.cseiu@gmail.com>
2021-05-15 22:59:40 +0600

    define and use class method

--- class.js
+++ class.js
@@ -1,9 +1,24 @@
-class Car { //How to use a JavaScript Class.
+// class Car { //How to use a JavaScript Class.
+//     constructor(name, year) {
+//         this.name = name;
+//         this.year = year;
+//     }
+// }
+
+// myCar = new Car("Ford", 2014);
+// console.log(myCar.name + " " + myCar.year);
+
+class Car { //How to define and use a Class method.
     constructor(name, year) {
         this.name = name;
         this.year = year;
+
+    }
+    age() {
+        let date = new Date();
+        return date.getFullYear() - this.year;
     }
 }
 
-myCar = new Car("Ford", 2014);
-console.log(myCar.name + " " + myCar.year);
+let myCar = new Car("Ford", 2010);
+console.log("My car is " + myCar.age() + " years old.");

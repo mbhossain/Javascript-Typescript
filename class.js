c82fabdf000b903d19cf015c92dd52a5a953835f
Mohammad Belal <king.cseiu@gmail.com>
2021-05-17 22:12:38 +0600

    inheritance

--- class.js
+++ class.js
@@ -8,17 +8,42 @@
 // myCar = new Car("Ford", 2014);
 // console.log(myCar.name + " " + myCar.year);
 
-class Car { //How to define and use a Class method.
-    constructor(name, year) {
-        this.name = name;
-        this.year = year;
+// class Car { //How to define and use a Class method.
+//     constructor(name, year) {
+//         this.name = name;
+//         this.year = year;
 
+//     }
+//     age() {
+//         let date = new Date();
+//         return date.getFullYear() - this.year;
+//     }
+// }
+
+// let myCar = new Car("Ford", 2010);
+// console.log("My car is " + myCar.age() + " years old.");
+
+
+//class inheritance
+
+class Car {
+    constructor(brand) {
+        this.carname = brand;
     }
-    age() {
-        let date = new Date();
-        return date.getFullYear() - this.year;
+    present() {
+        return 'I have a ' + this.carname;
     }
 }
 
-let myCar = new Car("Ford", 2010);
-console.log("My car is " + myCar.age() + " years old.");
+class Model extends Car { //Use the "extends" keyword to inherit all methods from another class.
+    constructor(brand, mod) {
+        super(brand); //Use the "super" method to call the parent's constructor function.
+        this.model = mod;
+    }
+    show() {
+        return this.present() + ', it is a ' + this.model;
+    }
+}
+
+let myCar = new Model("Ford", "Mustang");
+console.log(myCar.show());

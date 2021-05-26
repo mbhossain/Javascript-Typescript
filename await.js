42888e068571039b3d0f212267a2d8a8fbe5dc82
Mohammad Belal <king.cseiu@gmail.com>
2021-05-26 21:37:14 +0600

    waiting for a timeout/await

--- await.js
+++ await.js
@@ -1,6 +1,17 @@
+// async function myDisplay() {
+//     let myPromise = new Promise(function(myResolve, myReject) {
+//         myResolve("I love You !!");
+//     });
+//     console.log(await myPromise);
+// }
+
+// myDisplay();
+
+
+//Waiting for a Timeout
 async function myDisplay() {
     let myPromise = new Promise(function(myResolve, myReject) {
-        myResolve("I love You !!");
+        setTimeout(function() { myResolve("I hate You !!"); }, 3000);
     });
     console.log(await myPromise);
 }

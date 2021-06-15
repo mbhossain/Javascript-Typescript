cc0e4ffe3f98b4ecadebaaf49be22e7ea5004ec7
Mohammad Belal <king.cseiu@gmail.com>
2021-06-15 23:46:51 +0600

    array method[splicing an array remove element]

--- array_method.js
+++ array_method.js
@@ -5,3 +5,11 @@ function myFunction() {
     fruits.splice(2, 0, "Lemon", "Kiwi");
     console.log("New Array:" + fruits);
 }
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits);
+
+function myFunction() {
+    fruits.splice(0, 1);
+    console.log(fruits);
+}

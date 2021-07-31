1ad6717b84f3c2a3d3919e1d91ee9083b4d602cc
Mohammad Belal <king.cseiu@gmail.com>
2021-07-31 14:01:27 +0600

    variable execution context

---
+++ jsAdvance/088_execution_context_variable.js
@@ -0,0 +1,23 @@
+var a = 10
+var b
+
+console.log(a)
+console.log(b)
+
+b = 20
+console.log(b)
+
+console.log(c)
+
+var c = 30
+console.log(c)
+
+// creational phase
+// a = undefind
+// b = undefind
+// c = undefind
+
+// executional phase
+// a = 10
+// b = 20
+// c = 30

05988b936f8814af497cbbb75f1edd1ee01d177b
Mohammad Belal <king.cseiu@gmail.com>
2021-07-31 19:26:29 +0600

    function execution context

---
+++ jsAdvance/089_execution_context_function.js
@@ -0,0 +1,14 @@
+abc()
+
+function abc() {
+    console.log('I am function')
+}
+
+abc()
+
+// creational phase
+// abc = ref. 
+
+// executional phase
+// line 1 will get ref. of abc function
+// line 7 will also get ref. of abc function

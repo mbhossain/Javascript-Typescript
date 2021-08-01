932c1ab6cb5eab27df30dcba729f50a40ea97756
Mohammad Belal <king.cseiu@gmail.com>
2021-08-02 00:54:07 +0600

    oop intro

---
+++ jsAdvance/098_oop_intro.js
@@ -0,0 +1,33 @@
+//procedural
+
+var width = 10
+var height = 20
+
+function calculateArea(width, height) {
+    return width * height
+}
+
+function calculateRange(width, height) {
+    return 2 * (width + height)
+}
+
+var area = calculateArea(width, height)
+var range = calculateRange(width, height)
+
+//object oriented
+
+var rect = {
+    width: 10,
+    height: 20,
+
+    calculateArea: function() {
+        return this.width * this.height
+    },
+
+    calculateRange: function() {
+        return 2 * (this.width + this.height)
+    }
+}
+
+var area = rect.calculateArea()
+var range = rect.calculateRange()

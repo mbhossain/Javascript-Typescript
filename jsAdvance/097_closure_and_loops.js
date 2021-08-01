ac834b9aee3a801ea96c4d90435a35c2563eebac
Mohammad Belal <king.cseiu@gmail.com>
2021-08-02 00:20:08 +0600

    closure and loops

---
+++ jsAdvance/097_closure_and_loops.js
@@ -0,0 +1,16 @@
+//wrong code
+
+// for (i = 1; i <= 5; i++) {
+//     setTimeout(function() {
+//         console.log(i)
+//     }, 1000 * i)
+// }
+
+//right code
+for (i = 1; i <= 5; i++) {
+    (function(n) {
+        setTimeout(function() {
+            console.log(n)
+        }, 1000 * n)
+    })(i)
+}

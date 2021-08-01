d9c37728d4da22c721236e03abf3301ab9df4d5f
Mohammad Belal <king.cseiu@gmail.com>
2021-08-01 08:10:48 +0600

    scope, nested scope, scope chain

---
+++ jsAdvance/092_scope.js
@@ -0,0 +1,8 @@
+var x = 10
+
+function test() {
+    // var x = 20
+    console.log(x)
+}
+
+test()
---
+++ jsAdvance/093_nested_scope.js
@@ -0,0 +1,14 @@
+var x = 10
+
+function test() {
+    var x = 20
+        // console.log(x)
+    function nested() {
+        var y = 30
+        console.log(x)
+    }
+    // console.log(y)  //This will happen on the line ReferenceError: y is not defined
+    nested()
+}
+
+test()

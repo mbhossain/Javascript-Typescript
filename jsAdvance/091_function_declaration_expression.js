7939ca160168155dfbd3aa8a6235c91d6c5e9812
Mohammad Belal <king.cseiu@gmail.com>
2021-08-01 06:42:09 +0600

    function declaration vs expression

---
+++ jsAdvance/091_function_declaration_expression.js
@@ -0,0 +1,15 @@
+declaration()
+
+function declaration() {
+    console.log('I ma function declaration')
+}
+
+declaration()
+
+// newExpression() // This will happen on the line TypeError: newExpression is not a function
+
+var newExpression = function() {
+    console.log('I ma function expresion')
+}
+
+newExpression()

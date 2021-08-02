db2db2b47957abeac4e801acaa4d15597ee09e0e
Mohammad Belal <king.cseiu@gmail.com>
2021-08-02 12:27:45 +0600

    new keyword

---
+++ jsAdvance/chapter_twelve_oop/112_new_keyword.js
@@ -0,0 +1,29 @@
+var Rectangle = function(width, height) {
+    this.width = width
+    this.height = height
+
+    this.draw = function() {
+        console.log('I am a Rectangle')
+        this.printProperties()
+        console.log(this)
+    }
+
+    this.printProperties = function() {
+        console.log('My width is: ' + this.width)
+        console.log('My height is: ' + this.height)
+    }
+}
+
+// var rect = new Rectangle(10, 20)
+// rect.draw()
+
+function myNew(constructor) {
+    var obj = {}
+    Object.setPrototypeOf(obj, constructor.prototype)
+    var argsArray = Array.prototype.slice.apply(arguments)
+    constructor.apply(obj, argsArray.slice(1))
+    return obj
+}
+
+var rect = myNew(Rectangle, 10, 15)
+rect.draw()

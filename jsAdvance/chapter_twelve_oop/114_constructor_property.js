3ab4692f5fdf52c191406fe6c8c594f05e3b5b01
Mohammad Belal <king.cseiu@gmail.com>
2021-08-02 13:30:53 +0600

    constructor property

---
+++ jsAdvance/chapter_twelve_oop/114_constructor_property.js
@@ -0,0 +1,49 @@
+var createRect = function(width, height) {
+    return {
+        width: width,
+        height: height,
+
+        draw: function() {
+            console.log('I am a Rectangle')
+            this.printProperties()
+            console.log(this)
+        },
+        printProperties: function() {
+            console.log('My width is: ' + this.width)
+            console.log('My height is: ' + this.height)
+        }
+    }
+}
+
+var rect1 = createRect(10, 20)
+rect1.draw()
+
+var Rectangle = function(width, height) {
+    this.width = width
+    this.height = height
+
+    this.draw = function() {
+        console.log('I am a Rectangle')
+        this.printProperties()
+        console.log(this)
+    }
+    this.printProperties = function() {
+        console.log('My width is: ' + this.width)
+        console.log('My height is: ' + this.height)
+    }
+}
+
+var rect2 = new Rectangle(10, 20)
+rect2.draw()
+
+// console command
+console.log(rect1.constructor)
+console.log(rect2.constructor)
+
+var obj = new Object()
+console.log(obj.constructor)
+
+var str = new String('str')
+console.log(str.constructor)
+console.log(str)
+console.log('My String is ' + str)

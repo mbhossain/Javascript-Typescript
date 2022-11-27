b37742631e167916e7126f8b168b601632e137eb
mbhossain <king.cseiu@gmail.com>
2022-11-27 15:35:05 +0600

    javascript one liner

---
+++ Javascript/one-liner/script.js
@@ -0,0 +1,41 @@
+// 001. Shuffle Array
+const shuffleArray = (arr) => arr.sort(() => Math.random() - 0.5);
+
+// Testing
+const arr001 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(shuffleArray(arr001));
+
+// 002. Copy to Clipboard
+// const copyToClipboard = (text) => navigator.clipboard?.writeText && navigator.clipboard.writeText(text);
+
+// Testing
+// copyToClipboard("Text value");
+
+// 003. Unique Elements
+const getUnique = (arr) => [...new Set(arr)];
+
+// Testing
+const arr002 = [1, 1, 2, 3, 3, 4, 4, 4, 5, 5];
+console.log(getUnique(arr002));
+
+//004. Detect Dark Mode
+const isDarkMode = () =>
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+// Testing
+console.log(isDarkMode());
+
+//005. Scroll To Top
+const scrollToTop = (element) =>
+    element.scrollIntoView({ behavior: "smooth", block: "start" });
+
+//006. Scroll To Bottom
+const scrollToBottom = (element) =>
+    element.scrollIntoView({ behavior: "smooth", block: "end" });
+
+//007. Generate Random Color
+const generateRandomHexColor = () =>
+    `#${Math.floor(Math.random() * 0xffffff).toString(16)}`;
+
+document.querySelector("h1").style.color = generateRandomHexColor();

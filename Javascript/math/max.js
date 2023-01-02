112cbd28a33ccc1546cb3a1405642843bf188810
mbhossain <king.cseiu@gmail.com>
2023-01-02 21:53:30 +0600

    second element return from array

--- Javascript/math/max.js
+++ Javascript/math/max.js
@@ -1,14 +1,39 @@
 let arr = [3, 5, 78, 23, 2, 9];
 
-function myMax(array) {
+// function myMax(array) {
+//   let maxValue = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] > maxValue) {
+//       maxValue = array[i];
+//     }
+//   }
+//   return maxValue;
+// }
+
+// let max = myMax(arr);
+// console.log('max:', max);
+
+function mySecondMax(array) {
   let maxValue = 0;
   for (let i = 0; i < array.length; i++) {
     if (array[i] > maxValue) {
       maxValue = array[i];
     }
   }
-  return maxValue;
+  let index = array.indexOf(maxValue);
+  array.splice(index, 1);
+  let newArr = array;
+
+  let secondMaxValue = 0;
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i] > secondMaxValue) {
+      secondMaxValue = newArr[i];
+    }
+  }
+
+  return secondMaxValue;
 }
 
-let max = myMax(arr);
-console.log('max:', max);
+
+let secondMax = mySecondMax(arr);
+console.log('secondMax:', secondMax);

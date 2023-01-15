62cae2611fb6ec3007a30003dfa983c03f2bc996
mbhossain <king.cseiu@gmail.com>
2023-01-15 23:33:49 +0600

    excel index solved going on

--- Problem Solving/excel-index.js
+++ Problem Solving/excel-index.js
@@ -1,9 +1,12 @@
-const number = 703;
+const number = 704;
 
 function findExcelIndex(n) {
   const arr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   let index = '';
+
   if (n > 26) {
+    let newArr = [];
+
     while (n > 26) {
       let childN = Math.trunc(n / 26);
       let nextN = n % 26;
@@ -13,18 +16,24 @@ function findExcelIndex(n) {
         nextN = 26
       }
 
-      let newArr = [];
-      n = childN;
       // debugger
-      newArr.push(childN);
-      newArr.push(nextN);
-      const length = newArr.length;
-
-      for (let i = 0; i < length; i++) {
-        index += arr[newArr[i] - 1];
+      if (childN < 27) {
+        newArr.push(childN);
       }
-      return index;
+
+      newArr.push(nextN);
+      // newArr.reverse();
+      n = childN;
+
     }
+
+    const length = newArr.length;
+    for (let i = 0; i < length; i++) {
+      index += arr[newArr[i] - 1];
+    }
+
+    return index;
+
   } else {
     return arr[n - 1];
   }

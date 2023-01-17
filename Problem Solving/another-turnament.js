6776ecc4bba82f1d133ba622e725d0da6174569b
mbhossain <king.cseiu@gmail.com>
2023-01-17 23:14:09 +0600

    another turnament commit 2

--- Problem Solving/another-turnament.js
+++ Problem Solving/another-turnament.js
@@ -1,12 +1,14 @@
 // https://codeforces.com/problemset/problem/1783/C
 
 const oponent = 6;
-const time = 7;
+const time = 2;
 const timeArr = [1, 3, 2];
 
 function findPosition(o, t, arr) {
   let totalwin = 0;
   let winPossible = 0;
+  let index = 0; 
+  
   for (let i = 1; i < o; i++) {
     totalwin += i;
   }
@@ -16,15 +18,20 @@ function findPosition(o, t, arr) {
   });
 
   let length = sortArr.length;
-  let index = 0;
+  
   for (let j = index; j < length; j++) {
     index = j;
     winPossible += sortArr[j];
-    if (winPossible > t) {
+    debugger
+    if (winPossible >= t)
       break
-    }
+
+
   }
-  winPossible = winPossible - sortArr[index];
+
+  if (!(index = (length - 1))) {
+    winPossible = winPossible - sortArr[index];
+  }
   return winPossible;
 }
 

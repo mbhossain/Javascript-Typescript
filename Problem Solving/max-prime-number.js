32ab99a866ead2c77603d8b1002198d09093b76b
mbhossain <king.cseiu@gmail.com>
2023-01-19 18:14:37 +0600

    max prime number count going on

---
+++ Problem Solving/max-prime-number.js
@@ -0,0 +1,99 @@
+// https://www.hackerrank.com/challenges/leonardo-and-prime/problem?isFullScreen=false
+
+const number = BigInt(500);
+
+function primeCount(n) {
+
+  // let store = [], i, j, primes = [], maxPrime = 1, index = 0;
+
+  // for (i = 2; i <= n; ++i) {
+
+  //   if (!store[i]) {
+  //     primes.push(i);
+  //     maxPrime *= primes[index];
+
+  //     if (maxPrime > n) {
+  //       maxPrime = maxPrime / primes[index];
+  //       break;
+  //     }
+
+  //     index++
+
+  //     for (j = i << 1; j <= n; j += i) {
+  //       store[j] = true;
+  //     }
+
+  //   }
+  // }
+
+  // return maxPrime;
+
+
+
+
+
+
+  // let maxPrime = 1;
+  // let numberOfPrime = 0;
+
+  // for (let i = 2; i <= n; i++) {
+  //   let flag = 0;
+
+  //   // looping through 2 to user input number
+  //   for (let j = 2; j < i; j++) {
+  //     if (i % j == 0) {
+  //       flag = 1;
+  //       break;
+  //     }
+  //   }
+
+  //   // if number greater than 1 and not divisible by other numbers
+  //   if (i > 1 && flag == 0) {
+
+  //     maxPrime *= i;
+  //     numberOfPrime++;
+
+  //     if (maxPrime > n) {
+  //       numberOfPrime = numberOfPrime - 1;
+  //       break;
+  //     }
+  //     console.log(i);
+  //   }
+
+
+  // }
+  // return numberOfPrime;
+
+
+  let maxPrime = 1;
+  let numberOfPrime = 0;
+
+  for (let counter = 0; counter <= n; counter++) {
+    let notPrime = false;
+
+    for (var i = 2; i <= counter; i++) {
+      if (counter % i === 0 && i !== counter) {
+        notPrime = true;
+      }
+    }
+
+    if (counter > 1 && notPrime === false) {
+
+      maxPrime *= counter;
+      numberOfPrime++;
+      
+      if (maxPrime > n) {
+        numberOfPrime = numberOfPrime - 1;
+        break;
+      }
+      console.log(counter);
+    }
+
+  }
+
+  return numberOfPrime;
+
+}
+
+let result = primeCount(number);
+console.log('result:', result);

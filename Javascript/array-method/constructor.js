a421e5d5b7825c98f26b88cb3dabe46c5cd7e6d6
Billal <king.cseiu@gmail.com>
2025-01-31 09:04:15 +0600

    Array constructor

---
+++ Javascript/array-method/constructor.js
@@ -0,0 +1,23 @@
+const outputElement = document.getElementById('output');
+console.log = function (message) {
+  if (typeof message === 'object') {
+    outputElement.innerHTML += JSON.stringify(message, null, 2) + '\n';
+  } else {
+    outputElement.innerHTML += message + '\n';
+  }
+};
+
+// Syntax
+new Array();
+// new Array(element1);
+// new Array(element1, element2);
+
+Array();
+// Array(element1);
+// Array(element1, element2);
+
+const arrayEmpty = new Array(2);
+console.log(arrayEmpty.length); // 2
+console.log(arrayEmpty[0]); // undefined; actually, it is an empty slot
+console.log(0 in arrayEmpty); // false
+console.log(1 in arrayEmpty); // false
